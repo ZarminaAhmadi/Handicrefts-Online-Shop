62541745eb0078313a746fcee323ee41075f6570
Mosavi87 <sayedmahdi87@gmail.com>
2022-10-31 13:59:14 +0430

    lakdjfl

--- routes/web.php
+++ routes/web.php
@@ -68,4 +68,5 @@ Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function 
  Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand');
 
  Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
+ Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
  });
